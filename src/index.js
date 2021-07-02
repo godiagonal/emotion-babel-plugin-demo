15d3d11d3cc3247c63096f15ce3e73cdba446749
Samuel Johansson <@animech.com>
2021-07-02 15:19:21 +0200

    feat: add simpler example component

--- src/index.js
+++ src/index.js
@@ -1,16 +1,25 @@
-import React from "react";
 import ReactDOM from "react-dom";
-import { App } from "./App";
-import reportWebVitals from "./reportWebVitals";
+import { css } from "@emotion/react";
+import styled from "@emotion/styled";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+const Title = styled.h1`
+  font-size: 30px;
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  ${({ size }) =>
+    size === "large" &&
+    css`
+      font-size: 60px;
+    `}
+`;
+
+function App() {
+  return (
+    <div>
+      <Title>Regular title</Title>
+      <Title size="large">Large title</Title>
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
